0cbe4a86508433816fbfd90fad11678ba1622b7a
sidbdnzl <yk.f070114@gmail.com>
2026-07-09 12:20:35 +0900

    よくある質問ほぼ終わり

--- js/script.js
+++ js/script.js
@@ -5,3 +5,30 @@ hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     gnav.classList.toggle("active");
 });
+
+const swiper = new Swiper('.swiper', {
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: true,
+    },
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+
+
+document.querySelectorAll(".faq__item").forEach(item => {
+    const unit = item.querySelector(".faq__unit");
+    const icon = item.querySelector("i");
+
+    unit.addEventListener("click", () => {
+        item.classList.toggle("active");
+
+        if (item.classList.contains("active")) {
+            icon.classList.replace("fa-plus", "fa-minus");
+        } else {
+            icon.classList.replace("fa-minus", "fa-plus");
+        }
+    });
+});
